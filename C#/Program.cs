e45d6239db55e87d43a550a38c0d11f59e7a694b
anton <antonkovalev2019s@gmail.com>
2023-06-06 23:49:32 +0300

    commit

---
+++ C#/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+// Задача 1
+
+// int[] numbers = new int[15];
+// Random rnd = new Random();
+// int count = 0;
+// System.Console.WriteLine("Элементы массива:  ");
+// for (byte i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = rnd.Next(100, 1000);
+//         System.Console.WriteLine( + numbers[i]);
+
+//         if (numbers[i] % 2 == 0)
+//         {
+//             count++;
+//         }
+//     }
+
+// System.Console.WriteLine("Количество четных элементов массива: " + count);
+
+// Задача 2 
+
+// int[] numbers = new int[15];
+// Random rnd = new Random();
+// int sum = 0;
+// System.Console.WriteLine("Элементы массива:  ");
+// for (byte i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = rnd.Next(10, 100);
+//         System.Console.WriteLine( + numbers[i]);
+
+//         if (numbers[i] % 2 != 0)
+//         {
+//             sum += numbers[i];
+//         }
+//     }
+
+
+// System.Console.WriteLine("Сумма нечетных элементов массива: " + sum);
+
+// Задача 3
+
+// int[] numbers = new int[15];
+// Random rnd = new Random();
+// int numMAX = 0;
+// // int numMIN = 0;
+// System.Console.WriteLine("Элементы массива:  ");
+// for (byte i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = rnd.Next(1, 100);
+//         System.Console.WriteLine( + numbers[i]);
+
+//         if (numbers[i]>numbers[i+1])
+//         {
+//             numMAX = numbers[i];
+//         }
+//         // else
+//         //     numMIN = numbers[i];
+//     }
+// System.Console.WriteLine("Max " + numMAX);
+
+// в последней не разобрался
